304a3a046cf600894eb02945e313b4b776a56e1d
Jian Kim <jian7061@gmail.com>
2022-09-03 09:47:09 +0900

    add feature - follow user

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -2,7 +2,10 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { client, GET_PROFILES, GET_PUBLICATIONS } from "../../api";
 import Image from "next/image";
+import { ethers } from "ethers";
 
+import ABI from "../../abi.json";
+const address = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
 
 export default function Profile() {
   const router = useRouter();
@@ -38,9 +41,31 @@ export default function Profile() {
     }
   }
 
+  async function connect() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log({ accounts });
+  }
+
+  async function followUser() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(address, ABI, signer);
+
+    try {
+      const tx = await contract.foloow([id], [0x0]);
+      await tx.wait();
+      console.log("followed user successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   if (!profile) return null;
   return (
     <div>
+      <button onClick={connect}>Sign In</button>
       {profile?.picture ? (
         <Image
           src={profile.picture.original.url}
@@ -57,6 +82,7 @@ export default function Profile() {
         <p>{profile?.bio}</p>
         <p>Followers: {profile?.stats.totalFollowers}</p>
         <p>Followers: {profile?.stats.totalFollowing}</p>
+        <button onClick={followUser}>Follow</button>
       </div>
       <div>
         {publications.map((publication, index) => (
